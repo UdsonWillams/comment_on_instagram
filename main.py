87850bccbfe50d1330d73fda2e21ae511262c34d
UdsonWillams <udson.willams@gmail.com>
2020-11-01 15:05:10 -0300

    versão 1.0

--- main.py
+++ main.py
@@ -13,9 +13,17 @@ nome_usuario = dados[1]
 senha_usuario = dados[2]
 
 # selecionando perfis do banco para uma lista
+perfis = []
+perfilVarrido = []
 db.cursor.execute("SELECT perfisInsta FROM perfis")
 for perfil in db.cursor.fetchall():
     perfis.append(str(perfil))
+# varrendo lista retirando caracteres indesejados
+for p in perfis:
+    item = p
+    for i in ["(", "'", ",", ")"]:
+        item = item.replace(i, "")
+    perfilVarrido.append(item)
 
 options = webdriver.ChromeOptions()
 options.add_argument('lang=pt-br')
@@ -29,38 +37,22 @@ usuario = driver.find_element_by_name('username').send_keys(nome_usuario)
 sleep(2)
 
 senha = driver.find_element_by_name('password').send_keys(senha_usuario)
-
 sleep(1)
-pyautogui.press('enter')
+
+entrar = driver.find_element_by_class_name('sqdOP.L3NKy.y3zKF').click()
 sleep(5)
 driver.get(url)
 sleep(2)
 driver.fullscreen_window()
 sleep(1)
 
-for i in perfis:
+for i in perfilVarrido:
 
     comentarios = driver.find_element_by_class_name("X7cDz").click()
     sleep(1)
-    pyautogui.write(i, interval=0.2)
-    sleep(1)
-    pyautogui.press('backspace')
-    sleep(1)
-    pyautogui.press('backspace')
-    sleep(1)
-    pyautogui.press('home')
-    sleep(1)
-    pyautogui.press('delete')
-    sleep(1)
-    pyautogui.press('delete')
-    sleep(1)
-    pyautogui.press('end')
-    sleep(1)
-    pyautogui.press('enter')
-    sleep(1)
-    pyautogui.press('enter')
-    sleep(1)
-    pyautogui.press('enter')
+    pyautogui.write(i, interval=0.1)
     sleep(2)
+    publicar = driver.find_element_by_xpath("//*[@type='submit']").click()
+    sleep(60)
 
 driver.close()
